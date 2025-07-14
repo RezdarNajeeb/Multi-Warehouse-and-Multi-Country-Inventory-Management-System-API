d57de3f34304a2ff6bcaf39b1ec4d4839ed0afb8
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-07-14 17:00:07 +0300

    refactor migration methods to include return type declarations

--- database/migrations/2025_07_13_184800_create_inventories_table.php
+++ database/migrations/2025_07_13_184800_create_inventories_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up()
+    public function up(): void
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
@@ -21,7 +21,7 @@ return new class extends Migration {
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('inventories');
     }
